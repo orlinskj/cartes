308331b0bfa635378ab472a5f1069e31ed80523d
janek <orlinskj@linux.pl>
2017-10-20 09:52:05 +0200

    changed source tree

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var depInjObj = {}
 require('./intersection')(depInjObj)
 
 module.exports = {
-  vec2: require('./vec2'),
-  Line: require('./line')(depInjObj),
-  Segment: require('./segment')(depInjObj),
-  Ray: require('./ray')(depInjObj),
-  Circle: require('./circle')(depInjObj),
-  Corner: require('./corner')(depInjObj)
+  vec2: require('./lib/vec2'),
+  mat3: require('./lib/mat3'),
+  Line: require('./lib/geom/line')(depInjObj),
+  Segment: require('./lib/geom/segment')(depInjObj),
+  Ray: require('./lib/geom/ray')(depInjObj),
+  Circle: require('./lib/geom/circle')(depInjObj),
+  Corner: require('./lib/geom/corner')(depInjObj)
 }
